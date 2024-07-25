3e7e55a95953c77df847f76c5f6fc8b93b22da05
valdo <osvaldo@lvvdigital.com>
2024-07-24 19:39:03 -0600

    add new package microservices

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { envs } from './config';
 
@@ -18,6 +19,14 @@ async function bootstrap() {
     }),
   );
 
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natsServers,
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(envs.port);
 
   logger.log(`Payments-ms is running on port ${envs.port}`);
